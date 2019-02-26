f162f5a07fa1a897caef1f994a776f59580f3280
zszhere <zszhere@gmail.com>
2019-02-26 22:58:23 +0800

    实现新闻学习功能

--- init.py
+++ init.py
@@ -4,34 +4,57 @@ from selenium import webdriver
 from PIL import Image
 from io import BytesIO
 import base64 as b64
-import code  
+import code
+import time
+import random
 
-def login():
-    dr.get("https://pc.xuexi.cn/points/login.html")
-    Iframe = dr.find_elements_by_tag_name('iframe')[2]
-    dr.switch_to.frame(Iframe)
-    # Qrcode = dr.find_element_by_css_selector(".login_content>.login_body>.login_qrcode>.login_qrcode_content>img").get_attribute("src")
-    QrData = dr.find_element_by_css_selector(".login_qrcode_content>img").get_attribute("src")
-    print QrData
-    # QrStr = b64.b64decode(QrData.split(",")[-1])
-    # QrImg = Image.frombytes(QrStr)
-    # QrImg.show()
-    dr.switch_to.default_content()
-    raw_input()
+class XueXiQiangGuo:
+    def __init__(self, NewsNum, NewsTime):
+        self.NewsNum = NewsNum
+        self.NewsTime = NewsTime
+        self.dr = webdriver.Chrome()
 
-def learn():
-    dr.get("https://www.xuexi.cn/3695ce40a2f38ca24261ee28953ce822/9a3668c13f6e303932b5e0e100fc248b.html")
-    link = dr.find_elements_by_css_selector(".word-item")
-    for i in link:
-        print i.text
-    link[4].click()
-    link[6].click()
-    link[8].click()
-    link[10].click()
+    def login(self):
+        self.dr.get("https://pc.xuexi.cn/points/login.html")
+        Iframe = self.dr.find_elements_by_tag_name('iframe')[2]
+        self.dr.switch_to.frame(Iframe)
+        # QrData = self.dr.find_element_by_css_selector(".login_content>.login_body>.login_qrcode>.login_qrcode_content>img").get_attribute("src")
+        QrData = self.dr.find_element_by_css_selector(".login_qrcode_content>img").get_attribute("src")
+        # print QrData
+        # QrStr = b64.b64decode(QrData.split(",")[-1])
+        # QrImg = Image.frombytes(QrStr)
+        # QrImg.show()
+        self.dr.switch_to.default_content()
+        raw_input()
 
-    interp = code.InteractiveConsole(globals())  
-    interp.interact("")
+    def learn(self):
+        self.dr.get("https://www.xuexi.cn/3695ce40a2f38ca24261ee28953ce822/9a3668c13f6e303932b5e0e100fc248b.html")
+        link = self.dr.find_elements_by_id("Ca4gvo4bwg7400")
+        print "学习新闻:"
+        time.sleep(10) 
+        for i in range(0,self.NewsNum):
+            print link[i].text
+            link[i].click()   
+            time.sleep(10)   
+        MainPage = self.dr.current_window_handle
+        AllPage = self.dr.window_handles
+        # LearnPage = {}
+        for i in AllPage:
+            if i != MainPage:
+                self.dr.switch_to_window(i)
+                # LearnPage[i] = self.dr.execute_script('return document.body.scrollHeight')
+                self.dr.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+        time.sleep(self.NewsTime*60)
 
-dr = webdriver.Chrome()
-login()
-learn()
+        interp.interact("")
+
+    def quit(self):
+        self.dr.quit()
+
+if __name__ == "__main__":
+    interp = code.InteractiveConsole(globals())
+    # 每次打开6篇文章,打开4分钟
+    zz = XueXiQiangGuo(6,4)
+    zz.login()
+    zz.learn()
+    # zz.quit()
